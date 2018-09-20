f2a92fa35cd4b3e1648a4ebbb8eab5f9bf4f6e09
oxleberry <oxleberry@gmail.com>
2018-09-19 21:22:47 -0700

    added local time

--- app/app.js
+++ app/app.js
@@ -15,14 +15,69 @@ console.log('DISPLAYHEIGHT');
 console.log(displayHeight);
 
 // check for if scroll animation status
-let animator = false;
+let animatorID = false;
+
+
+function renderTime() {
+  // renders PST
+  let pacificTime = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"})
+  console.log(pacificTime);
+  // renders EST
+  let eastTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+  console.log(eastTime);
+
+  // splice after space
+  // splice backwards, omitting 3-6
+
+  // renders local time
+  let localDate = new Date()
+  let localHour = localDate.getHours()
+  let eastHour = localHour + 3;
+  let localMinutes = localDate.getMinutes()
+  let localAmpm = 'am';
+  let eastAmpm = 'am';
+
+  console.log(localDate);
+  console.log(localMinutes);
+
+  // formats the minutes
+  if (localMinutes < 10) {
+    localMinutes = `0${localMinutes}`
+  }
+  // hour of PST
+  if (localHour > 12) {
+    localHour -= 12;
+    localAmpm = 'pm';
+  }
+  // hour of EST
+  console.log(eastHour);
+  if (eastHour >= 24) {
+    eastHour -= 12;
+    eastAmpm = 'am';
+  } else if (eastHour > 12) {
+    eastHour -= 12;
+    eastAmpm = 'pm';
+  }
+
+  // updates the time in the DOM
+  let localTimes = document.querySelectorAll('.localTime');
+  localTimes.forEach(localTime => {
+    localTime.textContent = `${localHour}:${localMinutes}${localAmpm}`;
+  });
+  let eastTime2 = document.querySelector('.eastTime');
+  eastTime2.textContent = `${eastHour}:${localMinutes}${eastAmpm}`;
+
+}
+
+renderTime();
 
 autoScrollTo = (ele) => {
+  renderTime();
   // check for if scroll animation is still on
   // incase a user switches targets midway through animation
-  if (animator) {
+  if (animatorID) {
     console.log("ANIMATING");
-      clearTimeout(animator);
+      clearTimeout(animatorID);
   }
   // calculates the current scroll position
   // from the top of the page in pixels
@@ -41,7 +96,7 @@ autoScrollTo = (ele) => {
   // console.log(scrollY);
 
   // recursively keeps rendering the scroll animation
-  animator = setTimeout(`autoScrollTo('${ele}')`, speed);
+  animatorID = setTimeout(`autoScrollTo('${ele}')`, speed);
 
   // check to see the new target is below previous target, then scroll down
   if (targetY > prevTargetY) {
@@ -52,7 +107,7 @@ autoScrollTo = (ele) => {
       prevTargetY = targetY;
       console.log('scrollY');
       console.log(scrollY);
-      clearTimeout(animator);
+      clearTimeout(animatorID);
     }
     // if target hasn't been reached, keep running the scroll animation
     else {
@@ -68,7 +123,7 @@ autoScrollTo = (ele) => {
       prevTargetY = targetY;
       console.log('scrollY');
       console.log(scrollY);
-      clearTimeout(animator);
+      clearTimeout(animatorID);
     }
     else {
       scrollY = currentY - distance
@@ -82,7 +137,7 @@ autoScrollTo = (ele) => {
 
 const scrollLinks = document.querySelectorAll(".scrollLink");
 
-scrollLinks.forEach((scrollLink, idx) => {
+scrollLinks.forEach(scrollLink => {
   scrollLink.addEventListener("click", function(e) {
     e.preventDefault();
     let scrollTarget = scrollLink.dataset.scroll;
